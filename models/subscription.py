0815134d85bba1607be50d4a01d8ef6f93a811df
Travis Hall <t.hall89d@gmail.com>
2021-10-25 14:26:44 -0400

    created models dir and files

---
+++ models/subscription.py
@@ -0,0 +1,8 @@
+class Subscription():
+    """defines what properties will be on an object representation of a subscription.
+    """
+
+    def __init__(self, follower_id, author_id, created_on):
+        self.follower_id = None
+        self.author_id = None
+        self.created_on = created_on
