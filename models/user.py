6d7c826dafb7eed86c2f45a1442698a2e2b21840
Alexander Schroyer <alexanderschroyer@gmail.com>
2021-10-25 14:23:36 -0500

    fetches users in postman

--- models/user.py
+++ models/user.py
@@ -5,7 +5,8 @@ class User():
     # special `self` parameter that every method on a class
     # needs as the first parameter.
 
-    def __init__(self, first_name, last_name, email, bio, username, password, profile_image_url, created_on, active):
+    def __init__(self, id, first_name, last_name, email, bio, username, password, profile_image_url, created_on, active):
+        self.id = id
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
---
+++ users/__init__.py
@@ -0,0 +1,1 @@
+from .users import get_all_users, get_single_user, create_user
