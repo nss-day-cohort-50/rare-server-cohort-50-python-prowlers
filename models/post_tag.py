ec9bcfc943ee93e175d090f1d7e5a4443b0300ca
Travis Hall <t.hall89d@gmail.com>
2021-10-28 12:54:45 -0400

    updated post request fns

--- models/post_tag.py
+++ models/post_tag.py
@@ -5,7 +5,8 @@ class PostTag():
     """defines what properties will be on an object representation of a PostTag.
     """
 
-    def __init__(self, tag_id, post_id):
+    def __init__(self, id,  tag_id, post_id):
+        self.id = id
         self.tag_id = tag_id
         self.post_id = post_id
         self.tag = None
